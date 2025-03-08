3e6129ddacca9c02394e05ef05c62409c10ef185
lucialv <lucia.alvrzt@gmail.com>
2025-03-08 18:46:38 +0100

    Removed unused packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
-
 import vercel from "@astrojs/vercel";
-
 import react from "@astrojs/react";
 
 export default defineConfig({
-  integrations: [mdx(), sitemap(), react()],
+  integrations: [react()],
   output: "server",
   vite: {
     plugins: [tailwindcss()],
--- src/consts.ts
+++
@@ -1,2 +0,0 @@
-export const SITE_TITLE = "Sa Palomera";
-export const SITE_DESCRIPTION = "Welcome to my website!";
