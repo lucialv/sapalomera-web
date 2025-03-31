f5161b12e02016a258ac670b190d39bf09dad67b
lucialv <lucia.alvrzt@gmail.com>
2025-03-31 19:25:11 +0200

    added SEO Meta Tags, edited Hero section and added politica de privacitat

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import tailwindcss from "@tailwindcss/vite";
 import vercel from "@astrojs/vercel";
 import react from "@astrojs/react";
 
+import sitemap from "@astrojs/sitemap";
+
 export default defineConfig({
   site: 'http://localhost:4321',
-  integrations: [react()],
+  integrations: [react(), sitemap()],
   output: "server",
   vite: {
     plugins: [tailwindcss()],
